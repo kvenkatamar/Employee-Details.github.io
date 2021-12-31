713d9621370c7f0b482b80f90ebefe550ab9b1bc
kvenkatamar <78538730+kvenkatamar@users.noreply.github.com>
2021-12-31 21:57:14 +0530

    Updated code

--- dynamicEventHandling.js
+++ dynamicEventHandling.js
@@ -1,4 +1,4 @@
-var validateData1 = (event, clas1) => {
+/*var validation1 = (event, clas1) => {
     if(event.charCode >= 48 && event.charCode <= 57) {
         document.querySelector(clas1).style.display = 'none';
         return true;
@@ -6,7 +6,7 @@ var validateData1 = (event, clas1) => {
     document.querySelector(clas1).style.display = 'block';
     return false;
 }
-var validateData2 = (event) => {
+var validation2 = (event) => {
     console.log(document.querySelector("#age").value);
     if(document.querySelector("#age").value <= 18) {
         document.querySelector(".minAge").style.display = 'block';
@@ -16,4 +16,60 @@ var validateData2 = (event) => {
         document.querySelector(".minAge").style.display = 'none';
         return true;
     }
+}*/
+var validation1 = (event) => {
+    document.querySelector(".error-1").style.display = 'none';
+    return true;
+}
+var validation2 = (event) => {
+    document.querySelector(".error-2").style.display = 'none';
+    if(document.querySelector("#name").value == ''){
+        document.querySelector(".error-1").style.display = 'block';
+        return false;
+    }
+    return true;
+}
+var validation3 = (event) => {
+    document.querySelector(".error-3_1").style.display = 'none';
+    document.querySelector(".error-3_2").style.display = 'none';
+    document.querySelector(".error-3_3").style.display = 'none';
+    if(document.querySelector("#gender").value == ''){
+        document.querySelector(".error-2").style.display = 'block';
+        return false;
+    }
+    else if(event.charCode >= 48 && event.charCode <= 57){
+        document.querySelector(".error-3_2").style.display = 'none';
+        return true;
+    }
+    else{
+        document.querySelector(".error-3_2").style.display = 'block';
+        return false;
+    }
+}
+var validation4 = (event) => {
+    document.querySelector(".error-4").style.display = 'none';
+    if(document.querySelector("#age").value == ''){
+        document.querySelector(".error-3_1").style.display = 'block';
+        return false;
+    }
+    else if(document.querySelector("#age").value <= 18){
+        document.querySelector(".error-3_3").style.display = 'block';
+        return false;
+    }
+    else{
+        return true;
+    }
+    /*else if(document.querySelector("#age").value == ''){
+        document.querySelector(".error-3_3").style.display = 'block';
+        return false;
+    }*/
+}
+var validation5 = (event) => {
+    document.querySelector(".error-5_1").style.display = 'none';
+    document.querySelector(".error-5_2").style.display = 'none';
+    if(document.querySelector("#dep").value == ''){
+        document.querySelector(".error-4").style.display = 'block';
+        return false;
+    }
+    return true;
 }
